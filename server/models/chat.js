f65b2739d5a9b66bb333c97a6a10151948554975
adscript <adnanocode@gmail.com>
2019-10-22 02:18:18 +0700

    completed chat Models

---
+++ server/models/chat.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var chatSchema = new Schema({
+    id: String,
+    name: {
+        type: String,
+        require: true
+    },
+    chat: {
+        type: String,
+        require: true,
+    },
+});
+
+module.exports = mongoose.model('Chat', chatSchema);
