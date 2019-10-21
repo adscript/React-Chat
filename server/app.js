794e8fc632c518539bc6afddfea55d7de2267f03
adscript <adnanocode@gmail.com>
2019-10-22 02:20:14 +0700

    install mongoose and cors

--- server/app.js
+++ server/app.js
@@ -2,19 +2,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var chatsRouter = require('./routes/chats');
 
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/reactchat', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', chatsRouter);
 
 module.exports = app;
