315c219d5396aedcd1eca1b6a1200fb5e3fdff19
adscript <adnanocode@gmail.com>
2019-10-23 22:02:18 +0700

    chat schema completed

--- server-chat/models/chat.js
+++ server-chat/models/chat.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var chatSchema = new Schema({
-    id: String,
+    id: Number,
     name: {
         type: String,
         require: true
